7ee2919a93cfeda8497b18d8093f214048101fc9
alco-debug <taranovich@email.ua>
2020-04-12 16:01:50 +0300

    Urok2

---
+++ src/MaxMin.java
@@ -0,0 +1,33 @@
+public class MaxMin {
+    public static void main(String[] args) {
+        double[] arr = new double[10];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Math.random() * 69;
+            System.out.println(arr[i]);
+        }
+
+        double curmax = arr[0];
+        double curmin = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > curmax){
+                curmax = arr[i];
+            }
+
+            if(arr[i] < curmin){
+                curmin = arr[i];
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Наименьшее значение в массиве равно");
+        System.out.println(curmin);
+
+        System.out.println();
+
+        System.out.println("Наиольшее значение в массиве равно");
+        System.out.println(curmax);
+    }
+}
---
+++ src/SimpleNumber.java
@@ -0,0 +1,23 @@
+public class SimpleNumber {
+    public static void main(String[] args) {
+        int n = (int) (Math.random() * 100 + 1);
+
+        System.out.println(n);
+
+        boolean simple = true;
+
+        for(int i = 2; i < n / 2; i++){
+            if(n % i == 0){
+                simple = false;
+                break;
+            }
+        }
+
+        if(simple){
+            System.out.println("Число простое");
+        }
+        else{
+            System.out.println("Число не является простым");
+        }
+    }
+}
