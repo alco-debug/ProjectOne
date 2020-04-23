a482a2dd62d2124c5d6f2ddde2ac5cb59dce8090
alco-debug <taranovich@email.ua>
2020-04-23 20:30:49 +0300

    Lesson 5 - Human and Student

--- src/HumanStudent.java
+++ src/HumanStudent.java
@@ -1,5 +1,7 @@
 public class HumanStudent extends Human {
     int course;
+    private int[] marks = new int[10];
+    private double average;
 
     public HumanStudent(int age, int height, String fio, boolean married, int course) {
         super(age, height, fio, married);
@@ -26,5 +28,91 @@ public class HumanStudent extends Human {
                         String.valueOf(this.course) +
                         " course."
         );
+    }
+
+    public void toNextCourse() {
+        if (this.course > 0 && this.course < 4) {
+            this.course++;
+            this.age++;
+            this.study();
+            System.out.print(this.fio);
+            System.out.print(" has reached higher course (");
+            System.out.print(this.course);
+            System.out.println(").");
+        } else {
+            this.course = -1;
+            System.out.print(this.fio);
+            System.out.println(" can't reach higher course, he or she has already graduated.");
+        }
+    }
+
+    private void getAverageMark() {
+        double sum = 0;
+        for (int i = 0; i < this.marks.length; i++) {
+            sum += marks[i];
+        }
+        average = sum / this.marks.length;
+    }
+
+    private void study() {
+        this.fillMarks();
+        this.getAverageMark();
+    }
+
+    private void fillMarks() {
+        for (int i = 0; i < this.marks.length; i++) {
+            marks[i] = (int) (Math.random() * 9) + 1;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int newCourse) {
+        this.course = newCourse;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // self check
+        if (this == o)
+            return true;
+        // null check
+        if (o == null)
+            return false;
+        // type check and cast
+        if (getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        // field comparison
+        return (this.age == student.getAge()) && (this.course == student.getCourse()) && (this.fio == student.getFio());
+    }
+
+    @Override
+    public int hashCode() {
+        int res = 1;
+        res *= this.age * this.course;
+
+        for (int i = 0; i < this.fio.length(); i++) {
+            res *= (int) this.fio.charAt(i);
+        }
+        return res;
     }
 }
